d6ff1d26c1bb74933c7b5da3aff4e8d4595f00ca
ricardo <ricardoefamilia@gmail.com>
2022-04-07 16:48:35 -0300

    desafio NotacaoCientifica finalizado e enviado.

--- src/NotacaoCientifica.java
+++ src/NotacaoCientifica.java
@@ -2,206 +2,205 @@ import java.util.Scanner;
 
 public class NotacaoCientifica {
     public static void main(String[] args) {
-        //double numero = Math.random()*10;
         Scanner sc = new Scanner(System.in);
-        double numero = sc.nextDouble();
-        String numeroString = ""+numero;
-        int aux, ponto, cont = 0;
-        String expoente = "";
-        String aux2="", aux3, decimal = "", nc = "";
+        try {
+            Double numero = sc.nextDouble();
+            //System.out.println(numero);
+            String numString = ""+numero;
+            int virgula = numString.indexOf(".");
+            String mantissa = numString.substring(0,virgula);
+            String sinalMantissa = mantissa.substring(0,1);
+            String corpo = numString.substring(virgula+1);
+            String nc = "";
+            int E = corpo.indexOf("E");
 
-        ponto = numeroString.indexOf(".");
-        aux2 = numeroString.substring((ponto + 1),numeroString.length());
-        aux = aux2.indexOf("E");
-        if(aux != -1){
-            if(aux < 5){
-                expoente = aux2.substring((aux+1),aux2.length());
-                if(numero>0) {
-                    nc = "+" + numeroString.substring(0,ponto)+".";
-                    aux2 = aux2.substring(0,aux);
-                    if(expoente.length()>1){
-                        if(expoente.substring(0,1).equals("-")){
-                            nc += aux2+"000E"+expoente;
-                        }else{
-                            nc += aux2+"000E+"+expoente;
+            if(E != -1) {
+                String Efinal = corpo.substring(E);
+                String sinalE = Efinal.substring(1,2);
+                int semE = Integer.parseInt(Efinal.substring(1));
+                if(semE > 110) {
+                    System.out.println("*** Número inteiro acima de 110 caracteres ***");
+                    return;
+                }
+                String corpoSemE = corpo.substring(0,E);
+                if(semE < 10) {
+                    if(sinalE.charAt(0)=='-') {
+                        semE = Integer.parseInt(Efinal.substring(2));
+                        if(Efinal.length()>1) {
+                            Efinal = "E-"+semE;
+                        }else {
+                            Efinal = "E-0"+semE;
                         }
-                    }else{
-                        nc += aux2+"000E+0"+expoente;
+
+                    }else {
+                        if(Efinal.length()>1) {
+                            Efinal = "E+"+semE;
+                        }else {
+                            Efinal = "E+0"+semE;
+                        }
                     }
-                }else{
-                    nc = numeroString.substring(0,ponto)+".";
-                    if(Integer.parseInt(nc.substring(1,2))==0){
-                        aux2 = aux2.substring(0,aux);
-                        if(expoente.length()>1){
-                            if(expoente.substring(0,1).equals("-")){
-                                nc += aux2+"000E"+expoente;
-                            }else{
-                                nc += aux2+"000E+"+expoente;
-                            }
-                        }else{
-                            nc += aux2+"000E-0"+expoente;
+
+                }else {
+                    if(sinalE.charAt(0)=='-') {
+                        Efinal = "E-"+semE;
+                    }else {
+                        Efinal = "E+"+semE;
+                    }
+                }
+                int tamanhoCorpo = corpoSemE.length();
+                if(tamanhoCorpo > 4) {
+                    String quatro = corpo.substring(0,3);
+                    int quarto = Integer.parseInt(corpo.substring(3,4));
+                    int quinto = Integer.parseInt(corpo.substring(4,5));
+                    if(quinto >= 5) {
+                        quarto++;
+                    }
+                    corpo = quatro+quarto+Efinal;
+                }else {
+                    switch(tamanhoCorpo) {
+                        case 1: corpo = corpoSemE+"000"+Efinal; break;
+                        case 2: corpo = corpoSemE+"00"+Efinal; break;
+                        case 3: corpo = corpoSemE+"0"+Efinal; break;
+                        case 4: corpo = corpoSemE+Efinal; break;
+                    }
+                }
+            }else {
+                int tamanhoCorpo = corpo.length();
+                String semSinalMantissa = mantissa.substring(0,1);
+                if(sinalMantissa.charAt(0)=='-') {
+                    semSinalMantissa = mantissa.substring(1);
+                }else {
+                    semSinalMantissa = mantissa.substring(0);
+                }
+                if(semSinalMantissa.charAt(0)=='0') {
+                    int zeros = 0;
+                    int outros = 0;
+                    for(int x=0; x < tamanhoCorpo; x++) {
+                        if(corpo.charAt(x)=='0') {
+                            zeros++;
+                        }else {
+                            outros++;
+                            break;
                         }
-                    }else{
-                        aux2 = aux2.substring(0,aux);
-                        if(expoente.length()>1){
-                            if(expoente.substring(0,1).equals("-")){
-                                nc += aux2+"000E"+expoente;
-                            }else{
-                                nc += aux2+"000E+"+expoente;
-                            }
+                    }
+                    if(zeros > 0 && outros > 0){
+                        corpo = corpo.substring(zeros);
+                    }else if(zeros == 1 && outros == 0) {
+                        corpo = "0000E+00";
+                        if(sinalMantissa.charAt(0)!='-') {
+                            nc = "+"+mantissa+"."+corpo;
+                        }else {
+                            nc = mantissa+"."+corpo;
+                        }
+                        System.out.println(nc);
+                        System.exit(0);
+                    }
+                    if(sinalMantissa.charAt(0)=='-') {
+                        mantissa = sinalMantissa+corpo.substring(0,1);
+                    }else {
+                        mantissa = corpo.substring(0,1);
+                    }
+                    corpo = corpo.substring(1);
+                    tamanhoCorpo = corpo.length();
+                    if(tamanhoCorpo > 4) {
+                        String quatro = corpo.substring(0,3);
+                        int quarto = Integer.parseInt(corpo.substring(3,4));
+                        int quinto = Integer.parseInt(corpo.substring(4,5));
+                        if(quinto >= 5) {
+                            quarto++;
+                        }
+                        //corpo = quatro+quarto;
+                        if(zeros < 9) {
+                            corpo += "E-0"+(zeros+1);
                         }else{
-                            nc += aux2+"000E+0"+expoente;
+                            corpo += "E-"+(zeros+1);
+                        }
+
+                    }else {
+                        switch(tamanhoCorpo) {
+                            case 1: corpo += "000E-0"+(zeros+1); break;
+                            case 2: corpo += "00E-0"+(zeros+1); break;
+                            case 3: corpo += "0E-0"+(zeros+1); break;
+                            case 4: corpo += "E-0"+(zeros+1); break;
+                        }
+                    }
+                }else {
+                    if(tamanhoCorpo > 4) {
+                        int quatro = Integer.parseInt(corpo.substring(0,4));
+                        int quinto = Integer.parseInt(corpo.substring(4,5));
+                        if(quinto >= 5) {
+                            quatro++;
+                        }
+                        corpo = ""+quatro+"E+00";
+                    }else {
+                        switch(tamanhoCorpo) {
+                            case 1: corpo += "000"; break;
+                            case 2: corpo += "00"; break;
+                            case 3: corpo += "0"; break;
                         }
                     }
                 }
-            }else{
-                if(numero>0) {
-                    nc = "+" + numeroString.substring(0,ponto)+".";
-                    if(aux2.length()>4){
-                        aux = aux2.indexOf("E");
-                        expoente = aux2.substring((aux+1),aux2.length());
-                        if(aux != -1){
-                            if(aux < 9){
-                                nc += aux2.substring(0, 4) + "E+0" + expoente;
-                            }else{
-                                nc += aux2.substring(0, 4) + "E+" + expoente;
+            }
+            if(corpo.equals("0000")) {
+                if(sinalMantissa.charAt(0)=='-') {
+                    if(mantissa.length() > 2) {
+                        corpo = mantissa.substring(2);
+                        mantissa = mantissa.substring(0,2);
+                        if(corpo.length()>4) {
+                            String quatro = corpo.substring(0,3);
+                            int quarto = Integer.parseInt(corpo.substring(3,4));
+                            int quinto = Integer.parseInt(corpo.substring(4,5));
+                            if(quinto >= 5) {
+                                quarto++;
                             }
-                        }else{
-                            int u = Integer.parseInt(aux2.substring(4, 5));
-                            int pu = Integer.parseInt(aux2.substring(3, 4));
-                            if(u > 5){
-                                pu++;
-                                aux2 = aux2.substring(0,3)+pu;
+                            corpo = quatro+quarto+"E+04";
+                        }else {
+                            int aux = corpo.length();
+                            switch(aux) {
+                                case 1: corpo += "000E+01"; break;
+                                case 2: corpo += "00E+02"; break;
+                                case 3: corpo += "0E+03"; break;
+                                case 4: corpo += "E+04"; break;
                             }
-                            nc += aux2.substring(0, 4) + "E+00";
-                        }
-                    }else{
-                        aux = aux2.indexOf("E");
-                        nc += aux2.substring(0, aux) + "E+0" + aux2.substring((aux2.length()-1), aux2.length());
-                        nc = numeroString;
-                    }
-                }else {
-                    nc = numeroString.substring(0,ponto)+".";
-                    if(Integer.parseInt(nc.substring(1,2))==0){
-                        if(aux2.length()>4){
-                            aux = aux2.indexOf("E");
-                            if(aux != -1){
-                                if(aux < 9){
-                                    nc += aux2.substring(0, 4) + "E-0" + aux2.substring((aux+1), (aux + 2));
-                                }else{
-                                    nc += aux2.substring(0, 4) + "E-"  +aux2.substring(aux, aux2.length());
-                                }
-                            }
-                        }else{
-                            nc += aux2.substring(0, 4) + "E-0" + aux2.substring((aux2.length()-1), aux2.length());
-                            nc = numeroString;
                         }
                     }else {
-                        if(aux2.length()>4){
-                            aux = aux2.indexOf("E");
-                            if(aux != -1){
-                                if(aux < 9){
-                                    nc += aux2.substring(0, 4) + "E+0" + aux2.substring((aux+1), (aux + 2));
-                                }else{
-                                    nc += aux2.substring(0, 4) + "E+"  +aux2.substring(aux, aux2.length());
-                                }
-                            }
-                        }else{
-                            nc += aux2.substring(0, 4) + "E+0" + aux2.substring((aux2.length()-1), aux2.length());
-                            nc = numeroString;
-                        }
-                    }
-                }
-            }
-        }else{
-            if(numeroString.length()>6){
-                if(numero>0) {
-                    nc = "+" + numeroString.substring(0,ponto)+".";
-                    if(aux2.length()>4){
-                        aux = aux2.indexOf("E");
-                        if(aux != -1){
-                            if(aux < 9){
-                                nc += aux2.substring(0, 4) + "E+0" + aux2.substring((aux+1), (aux + 2));
-                            }else{
-                                nc += aux2.substring(0, 4) + "E+" + aux2.substring(aux, aux2.length());
-                            }
-                        }else{
-                            int u = Integer.parseInt(aux2.substring(4, 5));
-                            int pu = Integer.parseInt(aux2.substring(3, 4));
-                            if(u > 5){
-                                pu++;
-                            }
-                            aux2 = aux2.substring(0,3)+pu;
-                            nc += aux2.substring(0, 4) + "E+00";
-                        }
-                    }else{
-                        aux = aux2.indexOf("E");
-                        if(aux != -1){
-                            nc += aux2.substring(0, aux) + "E+0" + aux2.substring((aux2.length()-1), aux2.length());
-                        }else{
-                            nc = numeroString;
-                        }
+                        corpo += "E+00";
                     }
                 }else {
-                    nc = numeroString.substring(0,ponto)+".";
-                    if(nc.equals("-0.")){
-                        if(aux2.length()>4){
-                            aux = aux2.indexOf("E");
-                            if(aux != -1){
-                                if(aux < 9){
-                                    nc += aux2.substring(0, 4) + "E-0" + aux2.substring((aux+1), (aux + 2));
-                                }else{
-                                    nc += aux2.substring(0, 4) + "E-"  +aux2.substring(aux, aux2.length());
-                                }
-                            }else{
-                                int u = Integer.parseInt(aux2.substring(4, 5));
-                                int pu = Integer.parseInt(aux2.substring(3, 4));
-                                if(u > 5){
-                                    pu++;
-                                }
-                                aux2 = aux2.substring(0,3)+pu;
-                                nc += aux2.substring(0, 4) + "E-00";
+                    if(mantissa.length() > 1) {
+                        corpo = mantissa.substring(1);
+                        mantissa = mantissa.substring(0,1);
+                        if(corpo.length()>4) {
+                            String quatro = corpo.substring(0,3);
+                            int quarto = Integer.parseInt(corpo.substring(3,4));
+                            int quinto = Integer.parseInt(corpo.substring(4,5));
+                            if(quinto >= 5) {
+                                quarto++;
                             }
-                        }else{
-                            aux = aux2.indexOf("E");
-                            if(aux != -1){
-                                nc += aux2.substring(0, 4) + "E+0" + aux2.substring((aux2.length()-1), aux2.length());
-                            }else{
-                                nc = numeroString;
+                            corpo = quatro+quarto+"E+04";
+                        }else {
+                            int aux = corpo.length();
+                            switch(aux) {
+                                case 1: corpo += "000E+01"; break;
+                                case 2: corpo += "00E+02"; break;
+                                case 3: corpo += "0E+03"; break;
+                                case 4: corpo += "E+04"; break;
                             }
                         }
-                    }else{
-                        if(aux2.length()>4){
-                            aux = aux2.indexOf("E");
-                            if(aux != -1){
-                                if(aux < 9){
-                                    nc += aux2.substring(0, 4) + "E+0" + aux2.substring((aux+1), (aux + 2));
-                                }else{
-                                    nc += aux2.substring(0, 4) + "E+"  +aux2.substring(aux, aux2.length());
-                                }
-                            }else{
-                                int u = Integer.parseInt(aux2.substring(4, 5));
-                                int pu = Integer.parseInt(aux2.substring(3, 4));
-                                if(u > 5){
-                                    pu++;
-                                }
-                                aux2 = aux2.substring(0,3)+pu;
-                                nc += aux2.substring(0, 4) + "E+00";
-                            }
-                        }else{
-                            aux = aux2.indexOf("E");
-                            if(aux != -1){
-                                nc += aux2.substring(0, 4) + "E+0" + aux2.substring((aux2.length()-1), aux2.length());
-                            }else{
-                                nc = numeroString;
-                            }
-                        }
+                    }else {
+                        corpo += "E+00";
                     }
                 }
-            }else{
-                nc = numeroString;
             }
+            if(sinalMantissa.charAt(0)!='-') {
+                nc = "+"+mantissa+"."+corpo;
+            }else {
+                nc = mantissa+"."+corpo;
+            }
+
+            System.out.println(nc);
+        }catch(Exception e) {
+            e.printStackTrace();
         }
-    System.out.println(nc);
     }
 }
